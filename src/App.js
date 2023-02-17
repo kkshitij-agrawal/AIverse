30ecdd030357adf3ac89f379f65594e02709382c
Kshitij Agrawal <43761593+kkshitij-agrawal@users.noreply.github.com>
2023-02-17 21:05:41 +0000

    update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import "./App.css";
 import headline from "./Assets/justonemoreproject.svg";
 import mascot from "./Assets/mascot.png";
 import blogsCover from "./Assets/blogscover.svg";
+import appsCover from "./Assets/appBg.svg";
+import arrowIcon from "./Assets/arrow.svg";
 
 function App() {
   return (
     <div className="App">
       <div className="grid-container">
         <div className="grid-item big-item">
-          <div className="openItem">here</div>
+          <div className="openItem">
+            <img src={arrowIcon} />
+          </div>
           <div className="contentItem">
             <p>a designer, developer and AI trio ✨ </p>
             <img src={headline} />
@@ -20,9 +24,18 @@ function App() {
             </p>
           </div>
         </div>
-        <div className="grid-item"></div>
+        <div className="grid-item apps-item coverimg">
+          <div className="openItem">
+            <img src={arrowIcon} />
+          </div>
+          <div className="contentItem">
+            <img src={appsCover} />
+          </div>
+        </div>
         <div className="grid-item blogs-item coverimg">
-          <div className="openItem">here</div>
+          <div className="openItem">
+            <img src={arrowIcon} />
+          </div>
           <div className="contentItem">
             <img src={blogsCover} />
           </div>
