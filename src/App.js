2a98b242efd1f1d0a57609ced774ccb544cce6b2
Kshitij Agrawal <43761593+kkshitij-agrawal@users.noreply.github.com>
2023-02-17 19:11:10 +0000

    homepage update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import headline from "./Assets/justonemoreproject.svg";
+import mascot from "./Assets/mascot.png";
+import blogsCover from "./Assets/blogscover.svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="grid-container">
+        <div className="grid-item big-item">
+          <div className="openItem">here</div>
+          <div className="contentItem">
+            <p>a designer, developer and AI trio ✨ </p>
+            <img src={headline} />
+
+            <p>
+              taking on new challenges, riding the latest trends and building
+              for the future 🚀
+            </p>
+          </div>
+        </div>
+        <div className="grid-item"></div>
+        <div className="grid-item coverimg">
+          <div className="openItem">here</div>
+          <div className="contentItem">
+            <img src={blogsCover} />
+          </div>
+        </div>
+        <div className="grid-item big-item3-1 "></div>
+        <div className="grid-item"></div>
+        <div className="grid-item mascot-item coverimg">
+          <div className="contentItem">
+            <img src={mascot} />
+          </div>
+        </div>
+        <div className="grid-item"></div>
+        <div className="grid-item"></div>
+      </div>
     </div>
   );
 }
