d62b8d961be857b719a5fd54102da6caeba960d1
Kshitij Agrawal <43761593+kkshitij-agrawal@users.noreply.github.com>
2023-06-21 11:19:43 +0100

    twitter feature added

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import appsCover from "./Assets/appBg.svg";
 import arrowIcon from "./Assets/arrow.svg";
 import xtreakCover from "./Assets/xtreak1.svg";
 import newsCover from "./Assets/newscover.svg";
+import blogsSpiral from "./Assets/blogspiral.svg";
+import projectsGrid from "./Assets/projectsGrid.svg";
+
+import { Tweet, Follow, Timeline } from "react-twitter-widgets";
 
 export default function App() {
   return (
@@ -26,40 +30,63 @@ export default function App() {
             </p>
           </div>
         </div>
+
         <div className="grid-item apps-item coverimg">
           <div className="openItem">
             <img src={arrowIcon} alt="" />
           </div>
           <div className="contentItem">
+            {/* <img id="appGrid" src={projectsGrid} alt="" /> */}
             <img src={appsCover} alt="" />
           </div>
         </div>
+
         <div className="grid-item blogs-item coverimg">
           <div className="openItem">
             <img src={arrowIcon} alt="" />
           </div>
           <div className="contentItem">
+            <img id="blogSpiral" src={blogsSpiral} alt="" />
             <img src={blogsCover} alt="" />
           </div>
         </div>
+
         <div className="grid-item big-item3-1 "></div>
-        <div className="grid-item"></div>
+
+        <div className="grid-item twitter-item">
+          <div className="contentItem">
+            <Timeline
+              dataSource={{
+                sourceType: "url",
+                url: "https://twitter.com/withrebus",
+              }}
+              options={{ width: "340", chrome: "noheader" }}
+            />
+
+            {/* <Tweet tweetId="1671296157086015493" />
+            <Follow username="reactjs" options={{ size: "large" }} />;
+            <Tweet tweetId="1633202122341621762" /> */}
+          </div>
+        </div>
+
         <div className="grid-item mascot-item coverimg">
           <div className="contentItem">
             <img src={mascot} alt="" />
           </div>
         </div>
+
         <div className="grid-item xtreak-item">
           <div className="openItem">
-            <img src={arrowIcon} />
+            <img src={arrowIcon} alt="" />
           </div>
           <div className="contentItem">
             <p>100 days of building </p>
             <p>Started on 01 Feb 2023</p>
 
-            <img src={xtreakCover} />
+            <img src={xtreakCover} alt="" />
           </div>
         </div>
+
         <div className="grid-item newsletter-item">
           <div className="contentItem">
             <p>
