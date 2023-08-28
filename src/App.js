d63da9940fc89a018b5c476ccc3c87362eaa1a9e
Kshitij Agrawal <43761593+kkshitij-agrawal@users.noreply.github.com>
2023-08-28 00:48:10 +0000

    Update 5 files

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import xtreakCover from "./Assets/xtreak1.svg";
 import newsCover from "./Assets/newscover.svg";
 import blogsSpiral from "./Assets/blogspiral.svg";
 import wipSticker from "./Assets/wip.svg";
+import astronautSticker from "./Assets/astro-trans.png";
 
 import { Tweet } from "react-twitter-widgets";
 
@@ -20,12 +21,12 @@ export default function App() {
             <img src={arrowIcon} alt="" />
           </div>
           <div className="contentItem">
-            <p>staying relevant, riding the AI spaceship✨ </p>
+            <p>upskilling; riding the AI spaceship✨ </p>
             <img src={headline} alt="" />
 
             <p>
-             on a mission to unbundle current patterns, 
-            design and build for the future 🚀
+              on a mission to unbundle the current processes, design and build
+              for the future 🚀
             </p>
           </div>
         </div>
@@ -58,13 +59,14 @@ export default function App() {
 
         <div className="grid-item twitter-item">
           <div className="contentItem">
+            <Tweet tweetId="1688128073596559360" />
             <Tweet tweetId="1679059141694836736" />
           </div>
         </div>
 
         <div className="grid-item mascot-item coverimg">
           <div className="contentItem">
-           
+            <img src={astronautSticker} alt="" />
           </div>
         </div>
 
@@ -73,6 +75,12 @@ export default function App() {
             <img src={arrowIcon} alt="" />
           </div>
           <div className="contentItem">
+            {/* <iframe
+              src="https://xtreak.netlify.app/"
+              frameBorder="0"
+              width="100%"
+              height="100%"
+            /> */}
             <p>100 days of building </p>
             <p>Started on 01 Feb 2023</p>
 
@@ -85,14 +93,20 @@ export default function App() {
             <p>
               Sign up to get the <b>juiciest behind the scenes!</b>
             </p>
-            <input
-              type="text"
-              placeholder="Email address"
-              name="mail"
-              required
-            />
-            <input type="submit" value="Let's go!" />
 
+            <iframe
+              src="https://embeds.beehiiv.com/a3e9c954-5128-4d3d-9b41-ac9567390374?slim=true"
+              data-test-id="beehiiv-embed"
+              width="90%"
+              height="52"
+              frameBorder="0"
+              scrolling="no"
+              style={{
+                margin: "0",
+                borderRadius: "0px",
+                backgroundColor: "transparent",
+              }}
+            />
             <p>
               <i>
                 Only accepting the <b>first 50 people</b>
